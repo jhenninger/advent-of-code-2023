98837f6d4259641cf811cef768cc3d5e54032e46
Johannes Henninger <jhenninger@users.noreply.github.com>
2023-12-05 22:58:51 +0100

    Day 5: reverse

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,24 +1,31 @@
-use std::ops::RangeInclusive;
-
-struct Map {
-    ranges: Vec<(RangeInclusive<u64>, u64)>,
+struct Almanac {
+    maps: Vec<Vec<(u64, u64, u64)>>,
 }
 
-impl Map {
-    fn convert(&self, value: u64) -> u64 {
-        for (range, offset) in &self.ranges {
-            if range.contains(&value) {
-                return value - range.start() + offset;
-            }
-        }
+impl Almanac {
+    fn seed_to_location(&self, value: u64) -> u64 {
+        self.maps.iter().fold(value, |value, map| {
+            map.iter()
+                .find(|(_, src, len)| *src <= value && src + len >= value)
+                .map(|(dst, src, _)| value - src + dst)
+                .unwrap_or(value)
+        })
+    }
 
-        value
+    fn location_to_seed(&self, value: u64) -> u64 {
+        self.maps.iter().rev().fold(value, |value, map| {
+            map.iter()
+                .find(|(dst, _, len)| *dst <= value && dst + len >= value)
+                .map(|(dst, src, _)| value + src - dst)
+                .unwrap_or(value)
+        })
     }
 }
-fn main() {
-    let mut input = include_str!("../input").split("\n\n");
 
-    let seeds: Vec<u64> = input
+fn main() {
+    let mut blocks = include_str!("../input").split("\n\n");
+
+    let seeds: Vec<u64> = blocks
         .next()
         .unwrap()
         .strip_prefix("seeds: ")
@@ -27,39 +34,37 @@ fn main() {
         .map(|s| s.parse().unwrap())
         .collect();
 
-    let maps: Vec<_> = input
+    let maps = blocks
         .map(|block| {
-            let ranges = block
+            block
                 .lines()
                 .skip(1)
                 .map(|l| {
-                    let mut nums = l
+                    let nums: Vec<_> = l
                         .split_ascii_whitespace()
-                        .map(|i| i.parse::<u64>().unwrap());
-                    let dst = nums.next().unwrap();
-                    let src = nums.next().unwrap();
-                    let len = nums.next().unwrap();
-                    (src..=src + len, dst)
+                        .map(|i| i.parse::<u64>().unwrap())
+                        .collect();
+                    (nums[0], nums[1], nums[2])
                 })
-                .collect();
-
-            Map { ranges }
+                .collect()
         })
         .collect();
 
+    let almanac = Almanac { maps };
+
     let part_1 = seeds
         .iter()
-        .cloned()
-        .map(|s| maps.iter().fold(s, |s, m| m.convert(s)))
+        .map(|s| almanac.seed_to_location(*s))
         .min()
         .unwrap();
 
-    // BRUTE FORCE BABY
-    let part_2 = seeds
-        .chunks(2)
-        .flat_map(|c| c[0]..c[0] + c[1])
-        .map(|s| maps.iter().fold(s, |s, m| m.convert(s)))
-        .min()
+    let seed_ranges: Vec<_> = seeds.chunks(2).map(|c| c[0]..c[0] + c[1]).collect();
+
+    let part_2 = (0..)
+        .find(|l| {
+            let s = almanac.location_to_seed(*l);
+            seed_ranges.iter().any(|r| r.contains(&s))
+        })
         .unwrap();
 
     println!("Part 1: {part_1}\nPart 2: {part_2}");
